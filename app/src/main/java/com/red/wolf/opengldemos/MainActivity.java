1b9dfc6cc5df98a06145c8b7d88590e3331fde4b
RedWolf <li.hongchao@best3456.com>
2016-08-10 14:49:03 +0800

    OpenGlDemos工程创建成功

---
+++ app/src/main/java/com/red/wolf/opengldemos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.red.wolf.opengldemos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
